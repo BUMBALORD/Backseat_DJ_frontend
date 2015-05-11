5b81a8cc7075f52b0f45848ae370b275961a34ab
Katrina Eaton <katrina.eaton@gmail.com>
2015-05-11 15:39:45 -0700

    add basic FB vote functionality

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,1 +1,31 @@
 
+$(document).ready(function() {
+
+  var skipVotes = new Firebase("https://backseatdj.firebaseIO.com/skipVotes");
+
+  $("#skip").one("click", function(event){
+    skipVotes.transaction(function (current_value) {
+      return (current_value + 1);
+    })
+  })
+
+
+
+  var replayVotes = new Firebase("https://backseatdj.firebaseIO.com/replayVotes");
+
+  $("#replay").one("click", function(event){
+    replayVotes.transaction(function (current_value) {
+      return (current_value + 1);
+    })
+  })
+
+
+  // skipVotes.on("value", function(snapshot) {
+  //   var newPlaylist = snapshot.val();
+  //   console.log(newPlaylist.skipVotes);
+  //   // console.log("Genre: " + newPlaylist.genre);
+  // })
+
+
+
+});
