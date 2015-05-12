562dfe8bb4ac549424f5c6c23c3983482a664e93
lea3182 <lkim3182@gmail.com>
2015-05-11 20:37:19 -0700

    Removed ajax calls for playlist index

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,31 +1,22 @@
 require 'httparty'
-# get '/' do
-#   # @playlist = Playlist.find(1).songs
-#       ##placeholder for params[:id]
-#   erb :"playlists/index"
-# end
 
 get '/users/:user_id/playlists' do
-  @test = params[:user_id]
+ 
+  @user = params[:user_id]
+  response = HTTParty.get("http://localhost:3000/users/#{params[:user_id]}/playlists").parsed_response
+  @playlists = response["playlist"]
+  @current_user = response["user"]
+  
+
   erb :"playlists/index"
 end
 
 
 get '/users/:user_id/playlists/:id' do
-  p params
   @playlist_id = params[:id]
   @user =  params[:user_id]
-
-  response = HTTParty.get("http://localhost:3000/users/#{params[:user_id]}/playlists")
-  p "*" * 100
-  p response.parsed_response["data"]
-  p "*" * 100
-   response.parsed_response["data"].each do |xyz|
-    if xyz["id"].to_i == params[:id].to_i
-          p xyz
-          p "*" * 100
-    end
-  end
+  response = HTTParty.get("http://localhost:3000/users/#{params[:user_id]}/playlists").parsed_response
+  @playlist = response["playlist"]
 
   erb :"playlists/show"
 end
