aa908342886719043298524f5669376ec0c29c95
Katrina Eaton <katrina.eaton@gmail.com>
2015-05-12 18:55:09 -0700

    ghost in the machine

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,109 +1,61 @@
 
-$(document).ready(function() {
 
-  var counter = new Firebase("https://backseatdj.firebaseIO.com/votes/counter");
-  counter.set(1)
-  counter.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    currentCount = snapshot.val()
-  })
-
-// Up Vote! //
+var setVotes = function(){
 
   var upSkipVotes = new Firebase("https://backseatdj.firebaseIO.com/votes/upSkipVotes");
+  var downSkipVotes = new Firebase("https://backseatdj.firebaseIO.com/votes/downSkipVotes");
 
-  // var upVote = function() {
-    $("#up-skip").on("click", function(event){
-      if(currentCount === 1){
-        upSkipVotes.transaction(function (current_value) {
-          return (current_value + 1);
-        })
-      }
-      // counter.set(0)
-      // $("#down-skip").hide()
+  // Reset Votes //
+  console.log("Setting the votes")
+  downSkipVotes.set(0)
+  upSkipVotes.set(0)
+
+  $("#up-skip").on("click", function(){
+    upSkipVotes.transaction(function (current_value) {
+      debugger;
+      return (current_value + 1);
     })
-  // }
+  })
 
-  // upVote();
+// Display Skip Song Vote Count //
 
   upSkipVotes.on("value", function(snapshot) {
     console.log(snapshot.val());
     $("#skip-count-up").html(snapshot.val())
   })
 
-// Down Vote! //
+// Don't Skip Song //
 
-  var downSkipVotes = new Firebase("https://backseatdj.firebaseIO.com/votes/downSkipVotes");
-
-  // var downVote = function() {
-    $("#down-skip").on("click", function(event){
-      if (currentCount === 1){
-        downSkipVotes.transaction(function (current_value) {
-          return (current_value + 1);
-        })
-      }
-      // counter.set(0)
-      // $("#up-skip").hide()
+  $("#down-skip").on("click", function(){
+    downSkipVotes.transaction(function (current_value) {
+      debugger;
+      return (current_value + 1);
     })
-  // }
+  })
 
-  // downVote();
+// Display Don't Skip Song Vote Count //
 
   downSkipVotes.on("value", function(snapshot) {
     console.log(snapshot.val());
     $("#skip-count-down").html(snapshot.val())
   })
 
-// Vote Total! //
-
-  var voters = 5
+  // Total Skip Votes //
 
   upSkipVotes.on("value", function(snapshot1) {
     upVotes = snapshot1.val()
 
-    downSkipVotes.on("value", function(snapshot2) {
-      downVotes = snapshot2.val()
-      totalVotes = upVotes + downVotes
-      $("#skip-count-total").html(totalVotes)
+    // Total Don't Skip Votes //
 
-      if (totalVotes >= voters){
-
-        if (upVotes > downVotes){
-          //trigger skip song
-        } else if (upVotes === downVotes){
-          //trigger random song
-        }
-        // $("#up-skip").show()
-        // $("#down-skip").show()
-        downSkipVotes.set(0)
-        upSkipVotes.set(0)
-        // counter.set(1)
-        // upVote();
-        // downVote();
-      }
-
-    })
   })
+  downSkipVotes.on("value", function(snapshot2) {
+    downVotes = snapshot2.val()
 
+    // Total Votes //
 
-
-
-
-// ART! //
-
-  var art = new Firebase("https://backseatdj.firebaseIO.com/images");
-
-  $("#art-button").on("click", function(event){
-    art.set(
-      {
-        img: "https://40.media.tumblr.com/418ea4c78e283560ceafa9da889e1e20/tumblr_n1689e4PAE1swa88uo1_500.jpg"
-      }
-    )
   })
+  totalVotes = upVotes + downVotes
+  $("#skip-count-total").html(totalVotes)
+}
 
-  art.on("value", function(snapshot) {
-    imageUrl = snapshot.val()
-    $("#art").html('<img src="'+imageUrl.img+'">')
-  })
 
-});
