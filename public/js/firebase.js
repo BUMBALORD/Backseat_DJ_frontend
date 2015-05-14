df716d9fc337237f6afb483e7f1354faa26638be
DAVID HALLINAN <davidhallinan@DAVIDs-MacBook-Pro-2.local>
2015-05-13 19:23:45 -0700

    throwaway

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,7 @@
 // function firebase() {
 $(document).ready(function() {
+  var firebase = function(){
+
 
   var numOfPlayers = 5;
 
@@ -127,3 +129,13 @@ $(document).ready(function() {
 // $(document).ready(function() {
 //   firebase()
 });
+firebase()
+
+  var resetFirebase = new Firebase("https://backseatdj.firebaseIO.com/triggers/resetFirebase");
+
+  resetFirebase.on("value", function(snapshot) {
+  if (snapshot.val() === true){
+      firebase()
+      resetFirebase.set(false)
+    }
+  })
